15bcc2356a541e429fbbf4624007989bb56401a4
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-07 20:53:21 -0300

    criando rotas com react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./pages/Inicio";
+import SobreMim from "./pages/SobreMim";
 
 function App() {
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>}></Route>
+        <Route path="/sobremim" element={<SobreMim/>}></Route>
+        <Route path="*" element={<div>Pagina não encontrada</div>}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
