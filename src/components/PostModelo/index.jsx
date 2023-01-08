62187e00dead00da939a55c1e7a274947de28c58
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-08 00:24:51 -0300

    criado componente: PostModelo

---
+++ src/components/PostModelo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './PostModelo.module.css'
+
+export default function PostModelo(props) {
+  return (
+    <article className={styles.postModeloContainer}>
+        <div className={styles.fotoCapa} style={{ backgroundImage: `url(${props.fotoCapa})`}}/>
+        <h2 className={styles.titulo}>{props.titulo}</h2>
+        <div className={styles.postConteudoContainer}>{props.children}</div>
+    </article>
+  )
+}
