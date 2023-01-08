9b8f483ef95b2a785ca34d44fd8c54a86a15a82d
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-08 12:49:41 -0300

    dinamizando dados do post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import posts from 'assets/posts.json'
+import PostModelo from 'components/PostModelo';
 
 export default function Post() {
     const parametros = useParams();
-    console.log(parametros)
-
+    
+    const post = posts.find(post => {
+      return post.id == parametros.id
+    })
 
   return (
-    <h1>Post {parametros.id}</h1>
+    <PostModelo fotoCapa={`/assets/posts/${post.id}/capa.png`} titulo={post.titulo}>
+      <h1>Texto</h1>
+    </PostModelo>
   )
 }
