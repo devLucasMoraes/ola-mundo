0d77663b315d656c9da06de2556a9962e3c22ffb
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-08 14:23:56 -0300

    corrigindo warning

--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,7 @@ function AppRoutes() {
           <Route index element={<Inicio />} />
           <Route path="/sobremim" element={<SobreMim />} />
         </Route>
-        <Route path="post/:id" element={<Post />} />
+        <Route path="post/:id/*" element={<Post />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
       <Rodape />
