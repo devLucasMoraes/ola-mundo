d951572bfb2d45e9ef0c60f6b8beb9cd70498701
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-08 13:08:17 -0300

    ultilizando o react-markdown e aplicando estilo no Post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import posts from 'assets/posts.json'
 import PostModelo from 'components/PostModelo';
+import ReactMarkdown from 'react-markdown';
+import './Post.css'
 
 export default function Post() {
     const parametros = useParams();
@@ -12,7 +14,11 @@ export default function Post() {
 
   return (
     <PostModelo fotoCapa={`/assets/posts/${post.id}/capa.png`} titulo={post.titulo}>
-      <h1>Texto</h1>
+      <div className='post-markdown-container'>
+        <ReactMarkdown>
+          {post.texto}
+        </ReactMarkdown>
+      </div>
     </PostModelo>
   )
 }
