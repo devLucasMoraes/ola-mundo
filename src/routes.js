c83b0c0d9db6b98a870d4e26d5a010b37dc47a80
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-07 23:53:17 -0300

    nas rotas aninhadas o path das rotas filhas se forem iguais a do pai pode ser substituido por index

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ function AppRoutes() {
       <Menu />
       <Routes>
         <Route path="/" element={<PaginaPadrao/>}>
-          <Route path="/" element={<Inicio/>}></Route>
+          <Route index element={<Inicio/>}/>
           <Route path="/sobremim" element={<SobreMim/>}/>
         </Route>
         <Route path="*" element={<div>Pagina não encontrada</div>}/>
